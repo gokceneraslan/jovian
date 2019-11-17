76317d2817212d3d8244f506910a593629d1f053
Gökcen Eraslan <geraslan@broadinstitute.org>
2019-11-16 21:46:55 -0500

    Parameterize using parameters.yaml

--- run-docker.sh
+++ run-docker.sh
@@ -12,5 +12,5 @@ mkdir $output_folder $input_folder
 for pyfile in jovian-01-upstream-qc.py jovian-02a-downstream-qc.py jovian-02b-downstream-integrated.py jovian-03-DE.py; do
     ipynbfile=${pyfile/.py/.ipynb}
     jupytext --to notebook -o $input_folder/$ipynbfile $pyfile
-    papermill $input_folder/$ipynbfile $output_folder/${ipynbfile/.ipynb/-output.ipynb}
+    papermill -f parameters.yaml $input_folder/$ipynbfile $output_folder/${ipynbfile/.ipynb/-output.ipynb}
 done
