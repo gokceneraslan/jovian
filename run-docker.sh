4f8b82535a9d5e998952ce577b62ad1fa2efda0b
Gökcen Eraslan <geraslan@broadinstitute.org>
2019-11-16 22:21:30 -0500

    Fix report generation

--- run-docker.sh
+++ run-docker.sh
@@ -8,12 +8,12 @@ output_folder='output_notebooks'
 input_folder='input_notebooks'
 reports_folder='reports'
 
-mkdir $output_folder $input_folder
+mkdir $output_folder $input_folder $reports_folder
 
 for pyfile in jovian-01-upstream-qc.py jovian-02a-downstream-qc.py jovian-02b-downstream-integrated.py jovian-03-DE.py; do
     ipynbfile=${pyfile/.py/.ipynb}
     jupytext --to notebook -o $input_folder/$ipynbfile $pyfile
     papermill -f parameters.yaml $input_folder/$ipynbfile $output_folder/${ipynbfile/.ipynb/-output.ipynb}
-    jupyter nbconvert $output_folder/$ipynbfile --output ${ipynbfile/.ipynb/.html} --output-folder $reports_folder
-    jupyter nbconvert $output_folder/$ipynbfile --output ${ipynbfile/.ipynb/.pdf} --output-folder $reports_folder
+    jupyter nbconvert $output_folder/$ipynbfile --output ${ipynbfile/.ipynb/.html} --output-dir $reports_folder
+    jupyter nbconvert $output_folder/$ipynbfile --output ${ipynbfile/.ipynb/.pdf} --output-dir $reports_folder
 done
