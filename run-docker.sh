4dcc0234f1ea07cbd211b68ee2028da6c1535952
Gökcen Eraslan <geraslan@broadinstitute.org>
2019-11-16 23:47:38 -0500

    Add no-input to nbconvert

--- run-docker.sh
+++ run-docker.sh
@@ -14,6 +14,5 @@ for pyfile in jovian-01-upstream-qc.py jovian-02a-downstream-qc.py jovian-02b-do
     ipynbfile=${pyfile/.py/.ipynb}
     jupytext --to notebook -o $input_folder/$ipynbfile $pyfile
     papermill -f parameters.yaml $input_folder/$ipynbfile $output_folder/${ipynbfile}
-    jupyter nbconvert --output ${ipynbfile/.ipynb/.html} --output-dir $reports_folder --to html $output_folder/$ipynbfile
-    jupyter nbconvert --output ${ipynbfile/.ipynb/.pdf} --output-dir $reports_folder --to pdf $output_folder/$ipynbfile
+    jupyter nbconvert --output ${ipynbfile/.ipynb/.html} --output-dir $reports_folder --to html --no-input $output_folder/$ipynbfile
 done
