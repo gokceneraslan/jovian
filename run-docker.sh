8165c7e1d16bf9ba865c380cb7b59ea6b95e0a43
Gökcen Eraslan <geraslan@broadinstitute.org>
2019-11-16 20:59:34 -0500

    fix run-docker

--- run-docker.sh
+++ run-docker.sh
@@ -5,10 +5,12 @@ pushd data
 popd
 
 output_folder='output_notebooks'
-mkdir $output_folder
+input_folder='input_notebooks'
+
+mkdir $output_folder $input_folder
 
 for pyfile in jovian-01-upstream-qc.py jovian-02a-downstream-qc.py jovian-02b-downstream-integrated.py jovian-03-DE.py; do
-    jupytext --to notebook $pyfile
     ipynbfile=${pyfile/.py/.ipynb}
-    papermill $ipynbfile $output_folder/${ipynbfile/.ipynb/-output.ipynb}
+    jupytext --to notebook -o $input_folder/$ipynbfile $pyfile
+    papermill $input_folder/$ipynbfile $output_folder/${ipynbfile/.ipynb/-output.ipynb}
 done
