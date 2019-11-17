23fbf570005191a09c065c4c217a0b9368105e38
Gökcen Eraslan <geraslan@broadinstitute.org>
2019-11-16 22:00:30 -0500

    Add html/pdf conversion

--- run-docker.sh
+++ run-docker.sh
@@ -6,6 +6,7 @@ popd
 
 output_folder='output_notebooks'
 input_folder='input_notebooks'
+reports_folder='reports'
 
 mkdir $output_folder $input_folder
 
@@ -13,4 +14,6 @@ for pyfile in jovian-01-upstream-qc.py jovian-02a-downstream-qc.py jovian-02b-do
     ipynbfile=${pyfile/.py/.ipynb}
     jupytext --to notebook -o $input_folder/$ipynbfile $pyfile
     papermill -f parameters.yaml $input_folder/$ipynbfile $output_folder/${ipynbfile/.ipynb/-output.ipynb}
+    jupyter nbconvert $output_folder/$ipynbfile --output ${ipynbfile/.ipynb/.html} --output-folder $reports_folder
+    jupyter nbconvert $output_folder/$ipynbfile --output ${ipynbfile/.ipynb/.pdf} --output-folder $reports_folder
 done
