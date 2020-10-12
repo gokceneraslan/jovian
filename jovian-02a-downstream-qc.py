04ca754a48ce63874e762425f3fa01d2f4909f4f
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2020-10-12 14:49:20 -0400

    Use 50 PCs in scrublet

--- jovian-02a-downstream-qc.py
+++ jovian-02a-downstream-qc.py
@@ -130,7 +130,7 @@ import scrublet as scr
 
 def run_scrublet(ad):
     scrub = scr.Scrublet(ad.X)
-    n_prin_comps = min(min(ad.shape[0], ad.shape[1]), 30)
+    n_prin_comps = min(min(ad.shape[0], ad.shape[1]), 50)
     doublet_scores, predicted_doublets = scrub.scrub_doublets(verbose=False, n_prin_comps=n_prin_comps, min_counts=1, min_cells=0, log_transform=True)
 
     ad.obs['scrublet'] = predicted_doublets
